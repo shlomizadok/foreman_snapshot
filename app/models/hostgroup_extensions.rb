b8f3d0116f42a357ac1bbde2b6d152b9fb09a374
Shlomi Zadok <shlomi@ben-hanna.com>
2015-05-10 16:49:38 +0300

    Add timeout to snapshot! method

--- app/models/hostgroup_extensions.rb
+++ app/models/hostgroup_extensions.rb
@@ -1,3 +1,4 @@
+require 'timeout'
 module HostgroupExtensions
   extend ActiveSupport::Concern
 
@@ -8,20 +9,25 @@ module HostgroupExtensions
       # This could be run from Dynflow or directly, so we need
       # to use uncached{} to be able to detect the change in build state
       begin
-        if @host = ::Host::Managed.create!(host_hash)
-          until @host.build == false
-            logger.debug "Sleeping for host build state: #{@host.build}"
-            sleep 1
-            # Reload cache for next sleep check
-            @host = uncached { Host.find(@host.id) }
+        Timeout::timeout(600) do
+          if @host = ::Host::Managed.create!(host_hash)
+            until @host.build == false
+              logger.debug "Sleeping for host build state: #{@host.build}"
+              sleep(2)
+              # Reload cache for next sleep check
+              @host = uncached { Host.find(@host.id) }
+            end
           end
           logger.debug "Done waiting - #{@host.build}"
           image = @host.snapshot!
           raise unless @host.destroy
         end
-      rescue Exception => e
-        logger.debug e.message
-        raise e
+      rescue Timeout::Error => error
+        Rails.logger.debug "Could not complete building the host within time"
+        raise error
+      rescue Exception => error
+        Rails.logger.debug error.message
+        raise error
       end
 
       image
